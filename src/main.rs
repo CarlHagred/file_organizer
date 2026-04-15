d8f18a35537913f9c41a507323e8eb416305a0f9
carlhagred <carl.hagred@gmail.com>
2026-04-15 17:47:15 +0200

    feat: added reading of file from path

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 use std::env::args;
+use std::fs::read_dir;
 
-fn main() {
-    for argument in args() {
-        println!("{argument}");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let argument = match args().nth(1) {
+        Some(arg) => arg,
+        None => {
+            println!("A path needs to be provided as the first argument");
+            return Ok(());
+        }
+    };
+    let path_content = read_dir(argument)?;
+
+    for content in path_content {
+        let entry = content.unwrap();
+        let path = entry.path();
+        println!("{}", path.display());
     }
+
+    Ok(())
 }
+
