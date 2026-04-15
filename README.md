94cf66e6fb0b64588bc48a0f35889ca9983e486e
carlhagred <carl.hagred@gmail.com>
2026-04-15 22:22:43 +0200

    feat: implemented moving files in to folders

---
+++ README.md
@@ -0,0 +1,11 @@
+# File Organizer
+
+File Organizer is a small script for sorting directories
+
+## Get started
+
+Get started by running:
+
+```
+cargo run -- _file_path_
+```
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::env::args;
-use std::fs::read_dir;
+use std::fs::{create_dir_all, read_dir, rename};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let argument = match args().nth(1) {
@@ -9,14 +9,28 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Ok(());
         }
     };
-    let path_content = read_dir(argument)?;
-
+    let path_content = read_dir(&argument)?;
+    let base_dir = std::path::Path::new(&argument);
     for content in path_content {
         let entry = content.unwrap();
         let path = entry.path();
-        println!("{}", path.display());
+        if !path.is_file() {
+            continue;
+        }
+        if let Some(ext_str) = path.extension().and_then(|ext| ext.to_str()) {
+            let target_folder = match ext_str {
+                "jpg" | "png" | "gif" => "Images",
+                "pdf" | "doc" | "txt" => "Documents",
+                _ => "Other",
+            };
+            let target_dir_path = base_dir.join(target_folder);
+            create_dir_all(&target_dir_path)?;
+            let file_name = path.file_name().unwrap();
+            let destination_path = target_dir_path.join(file_name);
+            rename(&path, destination_path)?;
+            println!("Moved {} to {}", file_name.to_string_lossy(), target_folder);
+        }
     }
-
     Ok(())
 }
 
